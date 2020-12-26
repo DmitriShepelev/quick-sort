37e7a22cbad6998c336102722da11b472672e74d
dmitri_shepelev <shepelev.dm@gmail.com>
2020-12-26 18:00:46 +0300

    Done

--- QuickSort/Sorter.cs
+++ QuickSort/Sorter.cs
@@ -2,6 +2,7 @@
 
 // ReSharper disable InconsistentNaming
 #pragma warning disable SA1611
+#pragma warning disable CA1062
 
 namespace QuickSort
 {
@@ -12,8 +13,18 @@ namespace QuickSort
         /// </summary>
         public static void QuickSort(this int[] array)
         {
-            // TODO #1. Implement the method using a loop statements.
-            throw new NotImplementedException();
+            // #1. Implement the method using a loop statements.
+            if (array is null)
+            {
+                throw new ArgumentNullException($"Array can not be null.");
+            }
+
+            if (array.Length <= 1)
+            {
+                return;
+            }
+
+            QuickSortIterative(array, 0, array.Length - 1);
         }
 
         /// <summary>
@@ -21,8 +32,110 @@ namespace QuickSort
         /// </summary>
         public static void RecursiveQuickSort(this int[] array)
         {
-            // TODO #2. Implement the method using recursion algorithm.
-            throw new NotImplementedException();
+            // #2. Implement the method using recursion algorithm.
+            if (array is null)
+            {
+                throw new ArgumentNullException($"Array can not be null.");
+            }
+
+            if (array.Length == 0)
+            {
+                return;
+            }
+
+            QuickSortRecursive(array, 0, array.Length - 1);
+        }
+
+        private static void QuickSortIterative(int[] arr, int left, int right)
+        {
+            static void Swap(ref int x, ref int y)
+            {
+                int tmp = x;
+                x = y;
+                y = tmp;
+            }
+
+            static int Partition(int[] array, int left, int right)
+            {
+                int marker = left;
+                for (int i = left; i <= right; i++)
+                {
+                    if (array[i] < array[right])
+                    {
+                        Swap(ref array[marker], ref array[i]);
+                        marker++;
+                    }
+                }
+
+                Swap(ref array[marker], ref array[right]);
+
+                return marker;
+            }
+
+            int[] stack = new int[arr.Length];
+            int sp = -1; // Stack Pointer.
+
+            stack[++sp] = left;
+            stack[++sp] = right;
+
+            while (sp >= 0)
+            {
+                right = stack[sp--];
+                left = stack[sp--];
+                int pivot = Partition(arr, left, right);
+
+                if (pivot - 1 > left)
+                {
+                    stack[++sp] = left;
+                    stack[++sp] = pivot - 1;
+                }
+
+                if (pivot + 1 < right)
+                {
+                    stack[++sp] = pivot + 1;
+                    stack[++sp] = right;
+                }
+            }
+        }
+
+        private static void QuickSortRecursive(int[] array, int left, int right)
+        {
+            int leftMarker = left;
+            int rightMarker = right;
+            int pivot = array[(left + right) / 2];
+
+            while (leftMarker <= rightMarker)
+            {
+                while (array[leftMarker] < pivot)
+                {
+                    leftMarker++;
+                }
+
+                while (array[rightMarker] > pivot)
+                {
+                    rightMarker--;
+                }
+
+                if (leftMarker <= rightMarker)
+                {
+                    int tmp = array[leftMarker];
+                    array[leftMarker] = array[rightMarker];
+                    array[rightMarker] = tmp;
+
+                    leftMarker++;
+                    rightMarker--;
+                }
+            }
+
+            if (left < rightMarker)
+            {
+                QuickSortRecursive(array, left, rightMarker);
+            }
+
+            if (leftMarker < right)
+            {
+                QuickSortRecursive(array, leftMarker, right);
+            }
         }
     }
 }
